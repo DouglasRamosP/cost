4d9f77049e054c71f78562b8d95095bb532d1d32
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-05 16:52:00 -0300

    projetos e formulário

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,14 @@
+import ProjectForm from '../project/ProjectForm'
+import style from './NewProject.module.css'
+
 function NewProject(){
-    return <h1>NewProject</h1>
+    return(
+        <div className={style.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
+    )
 }
 
 export default NewProject
